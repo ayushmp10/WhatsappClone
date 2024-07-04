bc7730633cbcac0e5196deef47b3d5beb815aab8
ayushmp10 <ayushparambath@gmail.com>
2024-07-04 11:14:34 -0400

    Updated web screen - can now show contacts list

--- whatsapp_ui/lib/widgets/contacts_list.dart
+++ whatsapp_ui/lib/widgets/contacts_list.dart
@@ -8,45 +8,39 @@ class ContactsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Column(
+      padding: const EdgeInsets.only(top: 10),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return Column(
             children: [
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: info.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          title: Text(
-                            info[index]['name'].toString(),
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                          subtitle: Padding(
-                            padding: const EdgeInsets.only(top: 6),
-                            child: Text(
-                              info[index]['message'].toString(),
-                              style: const TextStyle(fontSize: 15),
-                            ),
-                          ),
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(info[index]['profilePic'].toString()),
-                          ),
-                          trailing: Text(
-                            info[index]['time'].toString(),
-                            style:
-                                const TextStyle(fontSize: 13, color: Colors.grey),
-                          ),
-                        ),
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 6),
+                      child: Text(
+                        info[index]['message'].toString(),
+                        style: const TextStyle(fontSize: 15),
                       ),
-                    );
-                  },
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(info[index]['profilePic'].toString()),
+                      radius: 30,
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(fontSize: 13, color: Colors.grey),
+                    ),
+                  ),
                 ),
               ),
               const Divider(
@@ -54,7 +48,9 @@ class ContactsList extends StatelessWidget {
                 indent: 85,
               ),
             ],
-          ),
-        ));
+          );
+        },
+      ),
+    );
   }
 }
